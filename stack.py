2289cd72b7a4e8ccff10cae63390824c95d0e907
satvik-choudhary <165299798+satvik-choudhary@users.noreply.github.com>
2024-06-10 22:10:23 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,6 +1,6 @@
 #there are 5 plates in weeding and named them 1 2 3 4 5 respectively 
 #a person come and taked a plate on top named 5 which is at last
-#this happen 2 more times means plate number 4 and plate and 3 are also taken away
+#this happen 2 more times means plate number 4 and plate number 3 are also taken away
 #this works on principle of last in first out(lifo)
 print("there are plate with number 1 2 3 4 5") 
 plate=[1,2,3,4,5]
