bee816e2ccf577415b13f569335314af95b6d679
satvik-choudhary <teenaraj.choudhary@gmail.com>
2024-06-14 22:53:42 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,11 +1,5 @@
-#there are 5 plates in weeding and named them 1 2 3 4 5 respectively 
-#a person come and taked a plate on top named 5 which is at last
-#this happen 2 more times means plate number 4 and plate and 3 are also taken away
-#this works on principle of last in first out(lifo)
-print("there are plate with number 1 2 3 4 5") 
 plate=[1,2,3,4,5]
-print("plate remove from stack of plate number is")
-print(plate.pop())
-print(plate.pop())
-print(plate.pop())
-print("plate remain",plate)
+plate_taken=plate[-1]
+plate=plate[:-1]
+print("plate taken is:",plate_taken)
+print("plate reamin in stack is:",plate)
